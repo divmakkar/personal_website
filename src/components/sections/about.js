befdf55ac7e671efddcc8978dd8ae9dbfaadfd8f
divmakkar <divu.mkr@gmail.com>
2023-11-16 18:34:22 +0900

    added about

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Python', 'Keras', 'PyTorch', 'AWS', 'SQL', 'React'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,21 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
+              Hi! I'm Divyansh, and I enjoy creating solutions to problems. My interests are
+              primarily in the field of Machine Learning and Deep Learning. I have also dabbled a
+              little with creating decentraiized applications using Ethereum Blockchain.
+            </p>
+            {/* Hello! My name is Brittany and I enjoy creating things that live on the internet. My
               interest in web development started back in 2012 when I decided to try editing custom
               Tumblr themes — turns out hacking together a custom reblog button taught me a lot
               about HTML &amp; CSS!
-            </p>
+            </p> */}
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              <a href="http://www.livesmart.co.jp/en/">LiveSmart</a>, an IoT based Smart Home
+              company and <a href="https://jp.seniorlife.io/">SeniorLife</a> a start-up dealing with
+              health assessments of seniors
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
